f41d3aaa2c31a742f4c262a3b2cac8e098104507
abseabse <sevapan@gmail.com>
2018-10-09 23:34:29 +0500

    minor

--- database_controller.py
+++ database_controller.py
@@ -1,7 +1,7 @@
 # Module to manupulate database
-# Version: 1
-# Date: 07.10.2018
-# Time: 21:24 GMT+5
+# Version: 2
+# Date: 09.10.2018
+# Time: 23:34 GMT+5
 
 # IMPORTS
 import sqlite3
@@ -19,6 +19,12 @@ def create_base(database_name):
             User_ID INTEGER PRIMARY KEY, 
             Username text
             )''')
+    c.execute('''CREATE TABLE UsersActivities (
+            Activity_ID INTEGER PRIMARY KEY,
+            User_ID integer,
+            Activity text,
+            FOREIGN KEY (User_ID) REFERENCES Users(User_ID)
+            )''')
     conn.commit()
     conn.close()
 
@@ -26,8 +32,8 @@ def nuke_base(database_name):
     # Wipes off created base
     conn = sqlite3.connect(database_name)
     c = conn.cursor()
-    c.execute('''pragma foreign_keys = on''')
-    c.execute('''DROP TABLE Users''')
+    c.execute('''DROP TABLE IF EXISTS UsersActivities''')
+    c.execute('''DROP TABLE IF EXISTS Users''')
     conn.commit()
     conn.close()
 
--- tests.py
+++ tests.py
@@ -1,6 +1,6 @@
-# Version: 1
-# Date: 07.10.2018
-# Time: 21:25 GMT+5
+# Version: 2
+# Date: 09.10.2018
+# Time: 23:32 GMT+5
 
 # IMPORTS
 import unittest
@@ -22,7 +22,7 @@ class Test_create_base(unittest.TestCase):
     # tests for create_base()
     
     def setUp(self):
-        pass
+        pass 
 
     def tearDown(self):
         database_controller.nuke_base(test_database)
@@ -53,9 +53,8 @@ class Test_nuke_base(unittest.TestCase):
         database_controller.nuke_base(test_database)
 
     def test_two(self):
-        # try to nuke an empty base, should be an error
-        with self.assertRaises(sqlite3.OperationalError):
-            database_controller.nuke_base(test_database)
+        # try to nuke an empty base, should not give an error
+        database_controller.nuke_base(test_database)
 
 
 class Test_create_base_table_tests(unittest.TestCase):
@@ -85,6 +84,38 @@ class Test_create_base_table_tests(unittest.TestCase):
         test_c.execute('''INSERT INTO Users VALUES (null, "goga")''')
         test_conn.commit()
         test_conn.close()
+
+    def test_three(self):
+        # try to insert a value in table UsersActivities. Should be
+        # an error, as the table has a foreign key in structure - User_ID
+        # and we have not entered any Users yet
+        with self.assertRaises(sqlite3.IntegrityError):
+            test_conn = sqlite3.connect(test_database)
+            test_c = test_conn.cursor()
+            test_c.execute('''pragma foreign_keys = on''')
+            test_c.execute('''INSERT INTO UsersActivities Values(
+                null, 
+                1, 
+                "vopya"
+                )''')
+            test_conn.commit()
+            test_conn.close()
+
+    def test_four(self):
+        # try to insert a pair of values in tables Users and UsersActivities. 
+        # (testing foreign key constraint) Should not give an error.
+        test_conn = sqlite3.connect(test_database)
+        test_c = test_conn.cursor()
+        test_c.execute('''pragma foreign_keys = on''')
+        test_c.execute('''INSERT INTO Users Values(null, "goga")''')
+        test_c.execute('''INSERT INTO UsersActivities Values(
+            null, 
+            1, 
+            "vopya"
+            )''')
+        test_conn.commit()
+        test_conn.close()
+
 
 # MAIN CYCLE
 if __name__ == '__main__':
